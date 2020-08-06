68ee0638627bac107317413960bd0cc981b8a706
3ecurityy <mamali6969>
2020-08-07 00:36:40 +0430

    snack Game

---
+++ src/ir/ariadeveloper/snackGame/Main.java
@@ -0,0 +1,28 @@
+package ir.ariadeveloper.snackGame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+        setResizable(false);
+        pack();
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Main();
+            ex.setVisible(true);
+        });
+    }
+}
